16b18ab84d9a5abfb52c705776111f91de708aec
Greg Hormann <ghormann@gmail.com>
2019-06-12 18:56:10 -0400

    Boot strap integration

--- frontend/src/components/SongList.vue
+++ frontend/src/components/SongList.vue
@@ -1,24 +1,22 @@
 <template>
-    <div>
-        <h2>Current Queue</h2>
-        <div class="songs">
-            <div v-for="song in allSongs" v-bind:key="song.id" class="song">
-                {{song.title}}
-
-            </div>
-        </div>
+  <!-- more at https://bootstrap-vue.js.org/docs/components/alert -->
+  <div>
+    <h2>Current Queue</h2>
+    <b-alert show>Default Alert</b-alert>
+    <div class="songs">
+      <div v-for="song in allSongs" v-bind:key="song.id" class="song">{{song.title}}</div>
     </div>
+  </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters } from "vuex";
 
 export default {
-    name: "SongList",
-    computed: mapGetters(['allSongs'])
-}
+  name: "SongList",
+  computed: mapGetters(["allSongs"])
+};
 </script>
 
 <style>
-
 </style>
