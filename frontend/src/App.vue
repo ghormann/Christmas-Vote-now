9a5eeb78d7d39df13fbea487cfc3634b1f4f1ff9
Greg Hormann <ghormann@gmail.com>
2019-06-14 19:29:04 -0400

    Added CoolDisplays Logo

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,18 +3,21 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <AvailSongList/>
     <OldSongList/>
+    <CoolDisplaysLogo/>
   </div>
 </template>
 
 <script>
 import AvailSongList from "./components/AvailSongList.vue";
 import OldSongList from "./components/OldSongList.vue";
+import CoolDisplaysLogo from "./components/CoolDisplaysLogo.vue";
 
 export default {
   name: "app",
   components: {
     AvailSongList,
-    OldSongList
+    OldSongList,
+    CoolDisplaysLogo
   }
 };
 </script>
---
+++ frontend/src/components/CoolDisplaysLogo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="coolLogo">
+    <a href="https://cooldisplays.net/index.php?page=single&id=1&width=400">
+      <b-img fluid src="https://cooldisplays.net/picture.php?logo=1&width=400"></b-img>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CoolDisplaysLogo"
+};
+</script>
+
+<style scoped>
+.coolLogo {
+  border: 2px;
+  border-style: solid;
+  border-radius: 25px;
+  margin: 2px;
+  background-color: #99ccff; /* Line is Optional */
+  display: inline-block;
+  padding: 5px 15px 5px 15px;
+  *display: inline; /* IE6 Hack */
+}
+img.coolLogo {
+  border: 0px;
+}
+</style>
