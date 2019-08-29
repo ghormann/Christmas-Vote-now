eb921b8349a73d0186867b78da10cb28884ae8ef
Greg Hormann <ghormann@thehormanns.net>
2019-08-29 22:31:26 +0000

    Docker container for server

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,7 @@ module.exports = [
     {
         method: "GET", path: "/",
         handler: (request, h) => {
+	    console.log("redirecting. Shouldn't be calling / ");
             return h.redirect("https://vote-now.org");
         }
     }
