45dc4d0a043f567b63135cbe455a326a15e1c278
Greg Hormann <ghormann@gmail.com>
2019-08-31 08:19:40 -0400

    Names to ordered list

--- frontend/src/components/NameQueue.vue
+++ frontend/src/components/NameQueue.vue
@@ -3,9 +3,9 @@
     <h2>Name Queue</h2>
     <div class="intro-text">Text your name to 513-854-1352 to become part of the display.</div>
     <div class="names">
-      <ul class="name-list">
+      <ol class="name-list">
         <li v-for="name in allNames" v-bind:key="name.id" class="name">{{name.name}}</li>
-      </ul>
+      </ol>
     </div>
   </div>
 </template>
