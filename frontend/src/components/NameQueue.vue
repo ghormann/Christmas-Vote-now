4d8a711ab1d2267bbb96bd7d54ee9d31c8c5da3e
Greg Hormann <ghormann@gmail.com>
2019-10-08 21:38:21 -0400

    Formating on name queue

--- frontend/src/components/NameQueue.vue
+++ frontend/src/components/NameQueue.vue
@@ -2,9 +2,15 @@
   <div class="outer">
     <h2>Name Queue</h2>
     <div class="intro-text">Text your first name to 513-854-1352 to become part of the display.</div>
+    <div class="alert" v-bind:class="errorClass" role="alert">Names in Green will be next song.</div>
+
     <div class="names">
       <ol class="name-list">
-        <li v-for="name in allNames" v-bind:key="name.id" class="name">{{name.name}}</li>
+        <li
+          v-for="name in allNames"
+          v-bind:key="name.id"
+          v-bind:class="{'name': true, 'low' : name.type === 'LOW', 'next' : name.type==='READY'}"
+        >{{name.name}}</li>
       </ol>
     </div>
   </div>
@@ -15,19 +21,34 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "NameQueue",
-  computed: mapGetters(["allNames"])
+  computed: {
+    ...mapGetters(["allNames", "currentSong"]),
+    errorClass: function() {
+      return {
+        "alert-success": true,
+        "d-none": this.currentSong.nameStatus != "READY"
+      };
+    }
+  }
 };
 </script>
 
 <style scoped>
-
 .names {
   display: flex;
   justify-content: center;
 }
 
+.next {
+  color: green;
+}
+
+.low {
+  color: gray;
+}
+
 .name {
-    text-align: left;
+  text-align: left;
 }
 
 .name-list {
