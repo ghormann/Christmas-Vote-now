0308ffd0e7f55b00bffd74048e3a6aeb0b6b28b1
Greg Hormann <ghormann@gmail.com>
2019-11-03 10:08:44 -0500

    Removed background on cooldisplays log

--- frontend/src/components/CoolDisplaysLogo.vue
+++ frontend/src/components/CoolDisplaysLogo.vue
@@ -14,11 +14,7 @@ export default {
 
 <style scoped>
 .coolLogo {
-  border: 2px;
-  border-style: solid;
-  border-radius: 25px;
   margin: 2px;
-  background-color: #99ccff; /* Line is Optional */
   display: inline-block;
   padding: 5px 15px 5px 15px;
   *display: inline; /* IE6 Hack */
