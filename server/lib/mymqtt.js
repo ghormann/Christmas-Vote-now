5844b15b6fff24dd755c867e0b2fdfcde22fe244
Greg Hormann <ghormann@gmail.com>
2020-08-02 13:07:07 -0400

    Start moving scheduler

--- server/lib/mymqtt.js
+++ server/lib/mymqtt.js
@@ -7,7 +7,7 @@ const myUtils = require("../lib/myUtils.js");
 
 const master_config = {
   send_enabled: false,
-  playGoodNight: false
+  playGoodNight: false,
 };
 var normalNames = [];
 var lowNames = [];
@@ -18,14 +18,12 @@ var last_send = 10;
 var last_intro = 10;
 var last_short_show = 10;
 var last_station = 10;
-var last_nameGen = moment()
-  .subtract(1, "day")
-  .toDate();
+var last_nameGen = moment().subtract(1, "day").toDate();
 
 var handlers = [
   {
     topic: "/christmas/nameQueue",
-    callback: function(topic, message) {
+    callback: function (topic, message) {
       datamodel.health.lastNameMqtt = moment().toDate();
       let data = JSON.parse(message.toString());
       normalNames = data.normal;
@@ -37,28 +35,51 @@ var handlers = [
         datamodel.health.lastnameGenereate = moment().toDate();
       }
       updateNames(true);
-    }
+    },
+  },
+  {
+    topic: "/christmas/scheduler/all_playlist",
+    callback: function (topic, message) {
+      let songs = JSON.parse(message.toString());
+      songs.forEach(function (newSong) {
+        let found = false;
+        datamodel.songs.forEach(function (oldSong) {
+          if (oldSong.playlist === newSong.name) {
+            found = true;
+            oldSong.title = newSong.desc;
+          }
+        });
+        if (!found) {
+          // Need to add
+          let song = createNewSong();
+          song.title = newSong.desc;
+          song.playlist = newSong.name;
+          myUtils.sortSongs();
+          datamodel.songs.push(song);
+        }
+      });
+    },
   },
   {
     topic: "/christmas/clock",
-    callback: function(topic, message) {
+    callback: function (topic, message) {
       let data = parseInt(message.toString());
       datamodel.clock.time = data;
-    }
+    },
   },
   {
     topic: "/christmas/vote/stats",
-    callback: function(topic, message) {
+    callback: function (topic, message) {
       let data = JSON.parse(message.toString());
       datamodel.stats = data;
       datamodel.health.lastStats = moment().toDate();
       addSongnameToVotes();
       addSongnameToSongs();
-    }
+    },
   },
   {
     topic: "/christmas/vote/setShortList",
-    callback: function(topic, message) {
+    callback: function (topic, message) {
       let data = message.toString();
       if (data.toUpperCase() === "TRUE") {
         datamodel.current.isShortList = true;
@@ -73,11 +94,11 @@ var handlers = [
         " because of ",
         data
       );
-    }
+    },
   },
   {
     topic: "/christmas/vote/debug",
-    callback: function(topic, message) {
+    callback: function (topic, message) {
       let data = message.toString();
       if (data.toUpperCase() === "TRUE") {
         datamodel.current.debug = true;
@@ -91,29 +112,11 @@ var handlers = [
         " because of ",
         data
       );
-    }
-  },
-  {
-    topic: "/christmas/setActive",
-    callback: function(topic, message) {
-      let data = message.toString();
-      if (data.toUpperCase() === "TRUE") {
-        datamodel.current.enabled = true;
-      }
-      if (data.toUpperCase() === "FALSE") {
-        datamodel.current.enabled = false;
-      }
-      console.log(
-        "Changing active status to ",
-        datamodel.current.enabled,
-        " because of ",
-        data
-      );
-    }
+    },
   },
   {
     topic: "/christmas/falcon/player/FPP.hormann.local/playlist_details",
-    callback: function(topic, message) {
+    callback: function (topic, message) {
       let data = JSON.parse(message.toString());
       datamodel.health.lastFppDate = moment().toDate();
       datamodel.current.status = data.status;
@@ -126,25 +129,9 @@ var handlers = [
       if ("activePlaylists" in data) {
         // Convert playlist to nice name
         // and set vote count if currently playing
-        data.activePlaylists.forEach(function(e) {
-          datamodel.current.title = myUtils.nonStandardPlaylistTitles(e.name);
-          if (e.name === "Wish_Name") {
-            datamodel.current.title = "Showing Names";
-            if ("READY" == datamodel.current.nameStatus) {
-              sendNameAction("RESET");
-            }
-          }
-          if (e.name === "Midnight") {
-            datamodel.clock.startedMidnight = true;
-            datamodel.clock.startedPrep = false;
-            if ("READY_MIDNIGHT" == datamodel.current.nameStatus) {
-              sendNameAction("RESET");
-            }
-          }
-          if (e.name === "Midnight_Prep") {
-            datamodel.clock.startedPrep = true;
-          }
-          datamodel.songs.forEach(function(s) {
+        data.activePlaylists.forEach(function (e) {
+          datamodel.current.title = e.description;
+          datamodel.songs.forEach(function (s) {
             if (e.name === s.playlist) {
               datamodel.current.title = s.title;
               if (s.votes > 7) {
@@ -156,7 +143,7 @@ var handlers = [
           });
 
           if ("currentItems" in e) {
-            e.currentItems.forEach(function(i) {
+            e.currentItems.forEach(function (i) {
               datamodel.current.secondsRemaining = i.secondsRemaining;
               datamodel.current.secondsTotal = i.secondsTotal;
             });
@@ -164,26 +151,43 @@ var handlers = [
         });
       }
       fireCallbacks();
-    }
-  }
+    },
+  },
 ];
 
-function doSend(playlist) {
-  let diff = Date.now() - last_send;
-  if (diff < 3000) {
-    // Sent something recently. Wait a minute
-    return;
-  }
-  console.log("Sending " + playlist);
-  let topic =
-    "/christmas/falcon/player/FPP.hormann.local/set/playlist/" +
-    playlist +
-    "/start";
-  last_send = Date.now();
-  client.publish(topic, "1", {}, function(err) {
+/*
+ * creates template for now Song record.
+ */
+function createNewSong() {
+  let id = 0;
+  datamodel.songs.forEach(function (s) {
+    s.votes = s.votes  + 1;
+    if (s.id > id) {
+      id = s.id;
+    }
+  });
+
+  id = id + 1;
+  return {
+    id: id,
+    title: "Unknown",
+    votes: 10,
+    shortlist: false,
+    playlist: "Unknown",
+  };
+}
+
+function requestSongList() {
+  // If songs are not here, schedule another check. 
+  if (datamodel.songs.length == 0 ) {
+    console.log("WARNING: No avaiable songs");
+    setTimeout(requestSongList, 1500);
+  } 
+  let topic = "/christmas/scheduler/requestSongs";
+  let msg = "1";
+  client.publish(topic, msg, function(err) {
     if (err) {
-      console.log("Error publishing topic");
-      console.log(err);
+      console.log("Unalbe to publish Reqeust for new SOngs");
     }
   });
 }
@@ -192,11 +196,23 @@ function sendVote(id, session) {
   let topic = "/christmas/vote/add";
   let msg = JSON.stringify({
     id: id,
-    source: session
+    source: session,
   });
-  client.publish(topic, msg, function(err) {
+  client.publish(topic, msg, function (err) {
     if (err) {
       console.log("Error Publishing to " + topic);
+      console.log(err);
+    }
+  });
+}
+
+function sendSongQueue() {
+  let topic = "/christmas/vote/songQueue";
+  let msg = JSON.stringify(datamodel.songs);
+
+  client.publish(topic, msg, function (err) {
+    if (err) {
+      console.log("Error Publishing SongQueue to " + topic);
       console.log(err);
     }
   });
@@ -205,155 +221,12 @@ function sendVote(id, session) {
 function sendName(name) {
   console.log("Adding name to the queue " + name);
   let topic = "/christmas/personsName";
-  client.publish(topic, name, function(err) {
+  client.publish(topic, name, function (err) {
     if (err) {
       console.log("Error Publishing to " + topic);
       console.log(err);
     }
   });
-}
-
-function sendNameAction(msg) {
-  if (datamodel.clock.startedPrep && msg === "GENERATE") {
-    console.log("Skipping name genreeate because in Midnight prep");
-    return;
-  }
-  console.log("Calling Send name Action with " + msg);
-  let topic = "/christmas/nameAction";
-  client.publish(topic, msg, function(err) {
-    if (err) {
-      console.log("Error publishing topic");
-      console.log(err);
-    }
-  });
-}
-
-function getCurrentHour() {
-  let myTime = new Date().toLocaleString("en-US", {
-    timeZone: "America/New_York"
-  });
-  myTime = new Date(myTime);
-  return myTime.getHours();
-}
-
-function doSendCheck() {
-  datamodel.current.isDisplayHours = myUtils.isDisplayHours();
-  if (!datamodel.current.enabled) {
-    return;
-  }
-  if (!master_config.send_enabled) {
-    return;
-  }
-
-  // If very close, then force Midnight Prep
-  if (
-    datamodel.clock.time < 780 &&
-    datamodel.clock.time > 0 &&
-    !datamodel.clock.startedPrep
-  ) {
-    doSend("Midnight_Prep");
-  }
-
-  // If Just hit midnight, then play Midnight.
-  if (datamodel.clock.time <= 0 && !datamodel.clock.startedMidnight) {
-    doSend("Midnight");
-  }
-
-  // Check If time to genreeate Midnight Names
-  if (datamodel.clock.time < 200 && datamodel.clock.time > 0) {
-    if (
-      datamodel.current.nameStatus != "PENDING_MIDNIGHT" &&
-      datamodel.current.nameStatus != "READY_MIDNIGHT"
-    ) {
-      console.log(
-        "Genereating midnight because ",
-        datamodel.current.nameStatus
-      );
-      sendNameAction("GENERATE_MIDNIGHT");
-    }
-  }
-
-  // If not display hours
-  if (!datamodel.current.isDisplayHours) {
-    if (datamodel.current.status === "idle") {
-      let hour = getCurrentHour();
-      if (hour == 23 && master_config.playGoodNight) {
-        master_config.playGoodNight = false;
-        doSend("Good_Night");
-      }
-      if (hour > 8 && hour < 16) {
-        // During day
-        doSend("off");
-      }
-      if (hour < 5) {
-        // early Morning
-        doSend("off");
-      }
-    }
-    // Early abort!!!!
-    return;
-  } // End NOT display hours.
-
-  // check if Names needs updated
-  let diff = Date.now() - last_nameGen;
-  if (
-    "IDLE" == datamodel.current.nameStatus &&
-    (normalNames.length > 0 || lowNames.length > 0)
-  ) {
-    if (diff > 720000) {
-      // 12 minutes
-      console.log("Normal Generate names because of ", diff);
-      sendNameAction("GENERATE");
-    } else if (normalNames.length > 9 && diff > 420000) {
-      // 7 minutes
-      console.log("7 minute genreate names ", diff);
-      sendNameAction("GENERATE");
-    } else if (normalNames.length > 20 && diff > 240000) {
-      console.log("4 minute genreate names ", diff);
-      sendNameAction("GENERATE");
-    }
-  } else if (
-    normalNames.length == 0 &&
-    lowNames.length == 0 &&
-    "IDLE" == datamodel.current.nameStatus &&
-    diff > 2100000
-  ) {
-    // After 35 minutes with nothing in the queue, Generate.
-    sendNameAction("GENERATE");
-  }
-
-  let hour = getCurrentHour();
-
-  if (hour == 22) {
-    // reset the playGoodNight flag
-    master_config.playGoodNight = true;
-  }
-
-  if (datamodel.current.status === "idle") {
-    // If names are ready, we should always play them....
-    if ("READY" == datamodel.current.nameStatus) {
-      doSend("Wish_Name");
-      datamodel.health.lastnamePlay = moment().toDate();
-    } else if (Date.now() - last_intro > 900000) {
-      // 15 min
-      last_intro = Date.now();
-      doSend("Intro");
-    } else if (
-      datamodel.current.isShortList &&
-      Date.now() - last_short_show > 600000
-    ) {
-      // 10 minutes
-      last_short_show = Date.now();
-      doSend("Short_Show");
-    } else if (Date.now() - last_station > 300000) {
-      // 5 min
-      last_station = Date.now();
-      doSend("TuneTo");
-    } else {
-      let song = datamodel.songs[0];
-      doSend(song.playlist);
-    }
-  }
 }
 
 function addCallback(c) {
@@ -364,32 +237,32 @@ function updateNames(doBroadcast) {
   var allNames = [];
   var id = 1;
 
-  readyNames.forEach(function(n) {
+  readyNames.forEach(function (n) {
     allNames.push({
       id: id,
       name: n.name,
       ts: n.ts,
-      type: "READY"
+      type: "READY",
     });
     id += 1;
   });
 
-  normalNames.forEach(function(n) {
+  normalNames.forEach(function (n) {
     allNames.push({
       id: id,
       name: n.name,
       ts: n.ts,
-      type: "NORMAL"
+      type: "NORMAL",
     });
     id += 1;
   });
 
-  lowNames.forEach(function(n) {
+  lowNames.forEach(function (n) {
     allNames.push({
       id: id,
       name: n.name,
       ts: n.ts,
-      type: "LOW"
+      type: "LOW",
     });
     id += 1;
   });
@@ -402,7 +275,7 @@ function updateNames(doBroadcast) {
 }
 
 function fireCallbacks() {
-  callbacks.forEach(function(c) {
+  callbacks.forEach(function (c) {
     c(datamodel);
   });
 }
@@ -427,33 +300,31 @@ function init() {
     //protocol: "mqtts",
     protocol: "mqtt",
     //ca: CA,
-    clientId:
-      "vote_" +
-      Math.random()
-        .toString(16)
-        .substr(2, 8),
+    clientId: "vote_" + Math.random().toString(16).substr(2, 8),
     //secureProtocol: "TLSv1_2_method",
     protocolId: "MQIsdp",
-    protocolVersion: 3
+    protocolVersion: 3,
   };
 
-  setInterval(doSendCheck, 500);
+  setInterval(sendSongQueue, 2000);
 
   client = mqtt.connect(options);
-  client.on("connect", function() {
+  client.on("connect", function () {
     console.log("MQTT Connect");
-    handlers.forEach(function(h) {
-      client.subscribe(h.topic, function(err) {
+    handlers.forEach(function (h) {
+      client.subscribe(h.topic, function (err) {
         if (err) {
           console.log("Failed to subscribe to ", h.topic);
         }
       });
     });
+    // Request song list
+    requestSongList();
   });
 
-  client.on("message", function(topic, message) {
+  client.on("message", function (topic, message) {
     let handled = false;
-    handlers.forEach(function(h) {
+    handlers.forEach(function (h) {
       if (topic == h.topic) {
         h.callback(topic, message);
         handled = true;
@@ -467,42 +338,42 @@ function init() {
 
 function addSongnameToVotes() {
   let songById = [];
-  datamodel.songs.forEach(function(s) {
+  datamodel.songs.forEach(function (s) {
     songById[s.id] = s.title;
   });
 
-  datamodel.stats.topSongs_1hr.forEach(function(i) {
+  datamodel.stats.topSongs_1hr.forEach(function (i) {
     i.title = songById[i.id];
   });
-  datamodel.stats.topSongs_24hr.forEach(function(i) {
+  datamodel.stats.topSongs_24hr.forEach(function (i) {
     i.title = songById[i.id];
   });
-  datamodel.stats.topSongs_year.forEach(function(i) {
+  datamodel.stats.topSongs_year.forEach(function (i) {
     i.title = songById[i.id];
   });
 }
 
 function addSongnameToSongs() {
   let songByPlaylist = {};
-  datamodel.songs.forEach(function(s) {
+  datamodel.songs.forEach(function (s) {
     songByPlaylist[s.playlist] = s.title;
   });
 
-  datamodel.stats.topPlayedSongs_year.forEach(function(i) {
+  datamodel.stats.topPlayedSongs_year.forEach(function (i) {
     if (i.name in songByPlaylist) {
       i.title = songByPlaylist[i.name];
     } else {
       i.title = myUtils.nonStandardPlaylistTitles(i.name);
     }
   });
-  datamodel.stats.topPlayedSongs_1hr.forEach(function(i) {
+  datamodel.stats.topPlayedSongs_1hr.forEach(function (i) {
     if (i.name in songByPlaylist) {
       i.title = songByPlaylist[i.name];
     } else {
       i.title = myUtils.nonStandardPlaylistTitles(i.name);
     }
   });
-  datamodel.stats.topPlayedSongs_24hr.forEach(function(i) {
+  datamodel.stats.topPlayedSongs_24hr.forEach(function (i) {
     if (i.name in songByPlaylist) {
       i.title = songByPlaylist[i.name];
     } else {
--- server/model/datamodel.js
+++ server/model/datamodel.js
@@ -1,103 +1,13 @@
-module.exports.songs = [
+module.exports.songs = [];
+  /*
   {
     id: 1,
     title: "Dance of the Sugar Plum Fairy",
     votes: 20,
     shortlist: false,
     playlist: "Sugar_Plum"
-  },
-  {
-    id: 2,
-    title: "EDM Carol of the Bells",
-    votes: 19,
-    shortlist: true,
-    playlist: "EDM"
-  },
-  {
-    id: 3,
-    title: "I Want a Hippopotamus for Christmas",
-    votes: 18,
-    shortlist: true,
-    playlist: "Hippo"
-  },
-  {
-    id: 4,
-    title: "House on Christmas Street",
-    votes: 17,
-    shortlist: false,
-    playlist: "Christmas_Street"
-  },
-  {
-    id: 5,
-    title: "Light of Christmas",
-    votes: 16,
-    shortlist: true,
-    playlist: "Light_of Christmas"
-  },
-  {
-    id: 6,
-    title: "12 Days of Christmas",
-    votes: 15,
-    shortlist: true,
-    playlist: "12_Days"
-  },
-  {
-    id: 7,
-    title: "Jingle Dogs",
-    votes: 14,
-    shortlist: false,
-    playlist: "Jingle_Dogs"
-  },
-  {
-    id: 8,
-    title: "I heard the bells on Christmas Day",
-    shortlist: false,
-    votes: 13,
-    playlist: "Heard_Bells"
-  },
-  {
-    id: 9,
-    title: "Little Drummer Boy",
-    shortlist: true,
-    votes: 12,
-    playlist: "Drummer"
-  },
-  {
-    id: 10,
-    title: "Amazing Grace",
-    shortlist: false,
-    votes: 11,
-    playlist: "Amazing_Grace"
-  },
-  {
-    id: 11,
-    title: "Christmas Minions",
-    votes: 10,
-    shortlist: false,
-    playlist: "Minions"
-  },
-  {
-    id: 12,
-    title: "Here comes Santa Claus",
-    votes: 29,
-    shortlist: false,
-    playlist: "Santa_Claus"
-  },
-  {
-    id: 13,
-    title: "Christmas Cannon",
-    votes: 28,
-    shortlist: false,
-    playlist: "Cannon"
-  },
-  {
-    id: 14,
-    title: "Joy to the World",
-    votes: 10,
-    shortlist: false,
-    playlist: "Joy"
   }
-];
+  */
 
 module.exports.nameQueue = [
   {
