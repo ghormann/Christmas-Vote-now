8d23893ba342a73d72ee66f378730355b451e028
Greg Hormann <ghormann@gmail.com>
2020-10-04 11:33:57 -0400

    Fixed typo

--- frontend/src/components/TodayPower.vue
+++ frontend/src/components/TodayPower.vue
@@ -1,7 +1,7 @@
 <template>
     <span>
       Today we've used {{powerStats.kwh}} KWh to power the lights costing about ${{powerStats.dollars}}. 
-      For the year, we've used {{stats.totalPower_year.kwh}} KWh to power the lights (or about ${{stats.totalPower_year.dollars}}.)
+      For the year, we've used {{stats.totalPower_year.kwh}} KWh to power the lights (or about ${{stats.totalPower_year.dollars}}).
     </span>
 </template>
 <script>
