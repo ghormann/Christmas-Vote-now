000cfcd501c75aa413782f721a2af9e6f7bc9a16
Greg Hormann <ghormann@gmail.com>
2020-12-22 15:43:18 -0500

    Other displays

--- frontend/src/components/OtherDisplays.vue
+++ frontend/src/components/OtherDisplays.vue
@@ -1,12 +1,35 @@
  <template>
   <div class="outer">
-    <h2>Near By Displays</h2>
+    <h2 class="gjh-padded">Near By Displays</h2>
+    <div class="gjh-padded">
+      Here are some other displays in the area worth a visit. Those with 🎵 are
+      also synchronized to music.
+    </div>
+    <b-container class="otherHouses">
+      <b-row
+        no-gutters
+        class="house-row"
+        v-for="house in otherDisplays"
+        v-bind:key="house.displayid"
+      >
+        <div class="col-6">
+          <a :href="house.url"><img :src="house.pict" class="img-fluid" /></a>
+        </div>
+        <div class="house-desc col-6" style="padding-left: 5px">
+          <div>
+            <a :href="house.url" class="house-name">{{ house.title }}</a>
+            <span v-if="house.musical"> 🎵 </span>
+          </div>
+          <div>{{ house.distance }} Miles from us</div>
+          <div>{{ house.city }}, {{ house.state }}</div>
+        </div>
+      </b-row>
+    </b-container>
   </div>
   <!--outer -->
 </template>
 <script>
 import { mapGetters, mapActions } from "vuex";
-
 
 export default {
   name: "OtherDisplays",
@@ -14,13 +37,24 @@ export default {
     ...mapActions(["fetchOtherDisplays"]),
   },
   computed: {
-    ...mapGetters(["currentSong", "lastUpdatedTime"]),
+    ...mapGetters(["otherDisplays"]),
   },
-    created() {
+  created() {
     this.fetchOtherDisplays();
-    }
+  },
 };
 </script>
 
 <style scoped>
+.house-desc {
+  text-align: left;
+  padding-left: 5px;
+}
+.house-name {
+  font-size: 1.5em;
+  text-decoration: underline;
+}
+.house-row {
+  margin-bottom: 2em;
+}
 </style>
--- frontend/src/store/modules/otherDisplays.js
+++ frontend/src/store/modules/otherDisplays.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const state = {
-  others: [],
+  others: [{ title: "Loading" }],
 };
 
 const getters = {
@@ -9,7 +9,9 @@ const getters = {
 };
 const actions = {
   async fetchOtherDisplays({ commit }) {
-    let r = await axios.get("https://cooldisplays.net/favoriteJson.php?u=4bc62e8f4304ef2058dd7bc867a38d55i85i3863e180845856d92dfbe72016bbbb2cab68f9b8");
+    let r = await axios.get(
+      "https://cooldisplays.net/favoriteJson.php?u=4bc62e8f4304ef2058dd7bc867a38d55i85i3863e180845856d92dfbe72016bbbb2cab68f9b8&lat=39.3953295&lng=-84.3991711"
+    );
     commit("setOthers", r.data);
   },
 };
@@ -17,6 +19,13 @@ const actions = {
 const mutations = {
   setOthers: (state, input) => {
     state.others = input.displays;
+    state.others.forEach(function(o) {
+      o.musical=o.tags.includes("Synced to Music");
+      o.distance = Math.round(o.distance * 100) / 100
+      o.pict =
+        "https://cooldisplays.net/picture.php?v=1608653961&width=300&display=" +
+        o.displayid;
+    });
   },
 };
 
