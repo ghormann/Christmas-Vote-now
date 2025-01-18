58d2999d51a60b5758990c3190edf236a58f71fc
Greg Hormann <ghormann@gmail.com>
2025-01-18 12:05:15 -0500

    Upgrade backend to Node 22

--- server/lib/session.js
+++ server/lib/session.js
@@ -1,4 +1,4 @@
-const uuidv4 = require("uuid/v4");
+const { v4: uuidv4 } = require('uuid');
 const myUtils = require("./myUtils.js");
 const datamodel = require("../model/datamodel.js");
 const maxVotes = 8;
